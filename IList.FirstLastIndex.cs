71dd44d6b410d8f84523d56ecaebed76586431ea
tonygiang <tony_s_giang@yahoo.com>
2022-07-27 15:46:48 +0700

    Project push.

---
+++ IList.FirstLastIndex.cs
@@ -0,0 +1,72 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IListFirstLastIndex
+  {
+    /// <summary>
+    /// Returns the index of the first element in a sequence that satisfies a
+    /// specified condition.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> to return an
+    /// element from.</param>
+    /// <param name="predicate">A function to test each element for a condition.
+    /// </param>
+    /// <returns>The index of the first element that satisfies a specified
+    /// condition.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="predicate"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">No element satisfies the condition in predicate.
+    /// 
+    /// -or-
+    ///
+    /// The source sequence is empty.</exception>
+    public static int FirstIndex<TSource>(this IEnumerable<TSource> source,
+      Func<TSource, bool> predicate)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (predicate == null) throw new ArgumentNullException("predicate");
+      int index = 0;
+      foreach (var e in source)
+      {
+        if (predicate(e)) return index;
+        ++index;
+      }
+      throw new InvalidOperationException("Sequence contains no matching element");
+    }
+
+    /// <summary>
+    /// Returns the index of the last element in a sequence that satisfies a
+    /// specified condition.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">An <see cref="IList{T}"/> to return an element
+    /// from.</param>
+    /// <param name="predicate">A function to test each element for a condition.
+    /// </param>
+    /// <returns>The index of the last element that satisfies a specified
+    /// condition.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="predicate"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">No element satisfies the condition in predicate.
+    /// 
+    /// -or-
+    ///
+    /// The source sequence is empty.</exception>
+    public static int LastIndex<TSource>(this IList<TSource> source,
+      Func<TSource, bool> predicate)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (predicate == null) throw new ArgumentNullException("predicate");
+      for (int i = source.Count - 1; i >= 0; --i)
+      { if (predicate(source[i])) return i; }
+      throw new InvalidOperationException("Sequence contains no matching element");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+﻿# CLSS.ExtensionMethods.IList.FirstLastIndex
+
+### Problem
+
+Sometimes, you have to find the index of an element that matches a condition within in a collection. This is done like this:
+
+```
+int firstMatchingIndex;
+for (int i = 0; i < collection.Count; ++i)
+{
+  if (!predicate(collection[i]) continue;
+  firstMatchingIndex = i; break;
+}
+```
+
+This takes a minimum of 6 lines of code that take some time to decode their purpose.
+
+`List<T>` has a [`FindIndex`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.findindex) method that shortens this syntax to a single line:
+
+```
+int firstMatchingIndex = collection.FindIndex(e => { /* predicate */ });
+```
+
+But this method is only available to `List<T>` and does not support finding the *last* matching index.
+
+### Solution
+
+This package provides `FirstIndex` and `LastIndex` to all [`IList<T>`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ilist-1) types to find the first and last index of an element matching a condition, respectively. Due to using manual index counting in its implementation, `FirstIndex` is also available to all `IEnumerable<T>` collections.
+
+Example:
+
+```
+using CLSS;
+
+var numbers = new int[] { 6, 4, -3, 15, -18, 9, 23 };
+var firstNegativeIdx = numbers.FirstIndex(n => n < 0); // 2
+var lastSingleDigitIdx = numbers.LastIndex(n => n > -10 && n < 10); // 5
+```
+
+**Note**: `ExclusiveSample` works on all types implementing the [`IList<T>`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ilist-1) interface, *including raw C# array*.
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).

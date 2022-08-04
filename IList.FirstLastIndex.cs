e0c61cc28252ca4c0141752c65267e5cc36222cb
tonygiang <tony_s_giang@yahoo.com>
2022-08-04 18:34:48 +0700

    Version 1.1
    - Reduce comment redundancy.

--- IList.FirstLastIndex.cs
+++ IList.FirstLastIndex.cs
@@ -44,21 +44,22 @@ namespace CLSS
     /// Returns the index of the last element in a sequence that satisfies a
     /// specified condition.
     /// </summary>
-    /// <typeparam name="TSource">The type of the elements of
-    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TSource">
+    /// <inheritdoc cref="FirstIndex{TSource}(IEnumerable{TSource}, Func{TSource, bool})"
+    /// path="/typeparam[@name='TSource']"/></typeparam>
     /// <param name="source">An <see cref="IList{T}"/> to return an element
     /// from.</param>
-    /// <param name="predicate">A function to test each element for a condition.
-    /// </param>
+    /// <param name="predicate">
+    /// <inheritdoc cref="FirstIndex{TSource}(IEnumerable{TSource}, Func{TSource, bool})"
+    /// path="/param[@name='predicate']"/></param>
     /// <returns>The index of the last element that satisfies a specified
     /// condition.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
-    /// <paramref name="predicate"/> is null.</exception>
-    /// <exception cref="InvalidOperationException">No element satisfies the condition in predicate.
-    /// 
-    /// -or-
-    ///
-    /// The source sequence is empty.</exception>
+    /// <exception cref="ArgumentNullException">
+    /// <inheritdoc cref="FirstIndex{TSource}(IEnumerable{TSource}, Func{TSource, bool})"
+    /// path="/exception[@cref='ArgumentNullException']"/></exception>
+    /// <exception cref="InvalidOperationException">
+    /// <inheritdoc cref="FirstIndex{TSource}(IEnumerable{TSource}, Func{TSource, bool})"
+    /// path="/exception[@cref='InvalidOperationException']"/></exception>
     public static int LastIndex<TSource>(this IList<TSource> source,
       Func<TSource, bool> predicate)
     {
